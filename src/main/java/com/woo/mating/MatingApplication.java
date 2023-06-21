16f2714bb1809c61edfd6d983d758076456865bd
woo <loo821921@gmail.com>
2023-06-21 23:06:59 +0900

    응애

--- src/main/java/com/woo/mating/MatingApplication.java
+++ src/main/java/com/woo/mating/MatingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MatingApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//응애앙응앵
         SpringApplication.run(MatingApplication.class, args);
     }
 
